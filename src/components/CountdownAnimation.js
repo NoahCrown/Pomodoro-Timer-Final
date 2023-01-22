8f7fc504fc1c32c6c8995b545682e44d8a5c86df
Charles Noah <noahimperium@gmail.com>
2023-01-22 22:16:04 +0800

    Update CountdownAnimation.js

--- src/components/CountdownAnimation.js
+++ src/components/CountdownAnimation.js
@@ -11,13 +11,13 @@ const CountdownAnimation = ({key, timer, animate, children}) => {
         isPlaying={animate}
         duration={timer * 60}
         colors={[
-          ['#FFFFFF', 0.33],
-          ['#FFFFFF', 0.33],
-          ['#FFFFFF', 0.33],
+          ['#FF6E31', 0.33],
+          ['#FF6E31', 0.33],
+          ['#FF6E31', 0.33],
         ]}
         strokeWidth={6}
         size={220}
-        trailColor="#F0997D"
+        trailColor="#D3756B"
         onComplete={ () => {
           stopAimate()
         }}
