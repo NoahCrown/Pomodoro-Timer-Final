336df1c613f4f94438a677279051682b9b465a1e
Charles Noah <noahimperium@gmail.com>
2023-01-22 21:47:58 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ const App = () => {
     setCurrentTimer,
     SettingsBtn } = useContext(SettingsContext)
 
-    useEffect(() => {updateExecute(executing)}, [executing, startAnimate],[])
+    useEffect(() => {
+      updateExecute(executing)
+    }, [executing, startAnimate])
+
 
   return (
     <div className="container">
