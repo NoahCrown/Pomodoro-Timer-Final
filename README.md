5d1b82c26470731706b5b097e3bca70d696e9246
Charles Noah <91674419+NoahCrown@users.noreply.github.com>
2023-01-22 21:04:28 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Pomodoro-Timer-Final
+# Pomodoro-StudyBuddy-App
+  a straightforward Pomodoro Timer application for desktop and mobile browsers. This aims to assist you in time management so that you can concentrate on any tasks, like studying
+
+* HTML
+* CSS
+* JavaScript
+* React
+* Rest API
+
+![localhost_3000_ (3)](https://user-images.githubusercontent.com/91674419/213916248-4245e163-41f3-4565-a958-ee073ce8e103.png)
+![localhost_3000_ (4)](https://user-images.githubusercontent.com/91674419/213916249-f9dedb25-25b4-490b-8909-d599490310d0.png)
---
+++ src/components/CountdownAnimation.js
@@ -0,0 +1,30 @@
+import { useContext } from 'react'
+import {CountdownCircleTimer} from 'react-countdown-circle-timer'
+import { SettingsContext } from '../context/SettingContext'
+const CountdownAnimation = ({key, timer, animate, children}) => {
+
+  const { stopAimate } = useContext(SettingsContext)
+
+    return (
+      <CountdownCircleTimer
+        key={key}
+        isPlaying={animate}
+        duration={timer * 60}
+        colors={[
+          ['#FF6E31', 0.33],
+          ['#FF6E31', 0.33],
+          ['#FF6E31', 0.33],
+        ]}
+        strokeWidth={6}
+        size={220}
+        trailColor="#D3756B"
+        onComplete={ () => {
+          stopAimate()
+        }}
+      >
+        {children}
+      </CountdownCircleTimer>
+    )
+}
+
+export default CountdownAnimation
